84fec42344f565b81a0a7ef31024ab765d59faae
Mathijs Verwer <v3rw3r@outlook.com>
2015-02-18 08:55:28 +0100

    Initial setup
    Initial setup of project

---
+++ EVNExport/EVNExport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EVNExport.MvcApplication" Language="C#" %>
---
+++ EVNExport/EVNExport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EVNExport.Startup))]
+namespace EVNExport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
